5c4f46ac541b02cfe548561a84d5ad95908091d6
rogov <rogovnikita2006@gmail.com>
2025-09-11 22:17:01 +0400

    Revert "rogov 1 lab"
    This reverts commit e88e7eafce084e8f67134204e7c7b8d734123bb7.

--- src/main/java/rogov/Task1/MyFirstProgram.java
+++
@@ -1,7 +0,0 @@
-package rogov.Task1;
-
-class MyFirstClass {
-    public static void main(String[] args) {
-        System.out.println("Hello world!!!");
-    }
-}
--- src/main/java/rogov/Task2/MyFirstProgram.java
+++
@@ -1,9 +0,0 @@
-package rogov.Task2;
-
-class MyFirstClass {
-    public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-    }
-}
