e4d2d68d441502cd0e0b7bc4e2f0a3f61c6b0423
rogov <rogovnikita2006@gmail.com>
2025-09-12 14:07:22 +0400

    Добавил interface MathFunction и реализовал его в IdentityFunction. Покрыл тестами

---
+++ src/main/java/ru/ssau/tk/phoenix/ooplabs/functions/IdentityFunctionTest.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.phoenix.ooplabs.functions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentityFunctionTest {
+
+    @org.junit.jupiter.api.Test
+    void apply() {
+        List<Double> actual = Arrays.asList(0., .1, Double.MAX_VALUE+1.);
+
+        IdentityFunction f = new IdentityFunction();
+        List<Double> expected = new ArrayList<>();
+        expected.add(f.apply(0.));
+        expected.add(f.apply(.1));
+        expected.add(f.apply(Double.MAX_VALUE+1.));
+
+        assertEquals(expected, actual);
+    }
+}
---
+++ src/main/java/ru/ssau/tk/phoenix/ooplabs/functions/MathFunction.java
@@ -0,0 +1,5 @@
+package ru.ssau.tk.phoenix.ooplabs.functions;
+
+public interface MathFunction {
+    double apply(double x);
+}
