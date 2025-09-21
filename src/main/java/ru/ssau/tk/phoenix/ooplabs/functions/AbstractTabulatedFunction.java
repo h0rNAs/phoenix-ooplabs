1401f3b65f2f11c1d1c3df1e9eb5dbe199ee1fd8
Kuzmin <egorvasekymin@gmail.com>
2025-09-21 12:16:43 +0400

    Написание AbstractTabulatedFunction

---
+++ src/main/java/ru/ssau/tk/phoenix/ooplabs/functions/AbstractTabulatedFunction.java
@@ -0,0 +1,29 @@
+package ru.ssau.tk.phoenix.ooplabs.functions;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction {
+
+    protected abstract int floorIndexOfX(double x);
+    protected abstract double extrapolateLeft(double x);
+    protected abstract double extrapolateRight(double x);
+    protected abstract double interpolate(double x, int floorIndex);
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return leftY + (rightY - leftY) * (x - leftX) / (rightX - leftX);
+    }
+
+    @Override
+    public double apply(double x) {
+        if (x < leftBound()) {
+            return extrapolateLeft(x);
+        } else if (x > rightBound()) {
+            return extrapolateRight(x);
+        } else {
+            int index = indexOfX(x);
+            if (index != -1) {
+                return getY(index);
+            } else {
+                return interpolate(x, floorIndexOfX(x));
+            }
+        }
+    }
+}
