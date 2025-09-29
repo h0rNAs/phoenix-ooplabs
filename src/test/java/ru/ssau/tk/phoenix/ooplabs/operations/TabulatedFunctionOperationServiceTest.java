eccf2bc9fd23200e5b53e94b7949ff7a44518ff2
Kuzmin <egorvasekymin@gmail.com>
2025-09-29 16:23:31 +0400

    Написание тестов для TabulatedFunctionOperationService

--- src/test/java/ru/ssau/tk/phoenix/ooplabs/operations/TabulatedFunctionOperationServiceTest.java
+++ src/test/java/ru/ssau/tk/phoenix/ooplabs/operations/TabulatedFunctionOperationServiceTest.java
@@ -1,6 +1,8 @@
 package ru.ssau.tk.phoenix.ooplabs.operations;
 
+import ru.ssau.tk.phoenix.ooplabs.exceptions.ArrayIsNotSortedException;
 import ru.ssau.tk.phoenix.ooplabs.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.phoenix.ooplabs.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.phoenix.ooplabs.functions.Point;
 import ru.ssau.tk.phoenix.ooplabs.functions.TabulatedFunction;
 import org.junit.jupiter.api.Test;
@@ -29,11 +31,12 @@ public class TabulatedFunctionOperationServiceTest {
     void testAsPointsWithSinglePoint() {
         double[] xValues = {5.0};
         double[] yValues = {10.0};
-        TabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
-        Point[] points = TabulatedFunctionOperationService.asPoints(function);
-        assertEquals(1, points.length);
-        assertEquals(5.0, points[0].x, 1e-10);
-        assertEquals(10.0, points[0].y, 1e-10);
+        assertThrows(IllegalArgumentException.class, () -> {
+            TabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
+        });
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new ArrayTabulatedFunction(xValues, yValues);
+        });
     }
 
     @Test
@@ -56,13 +59,61 @@ public class TabulatedFunctionOperationServiceTest {
     void testAsPointsOrderPreservation() {
         double[] xValues = {3.0, 1.0, 2.0};
         double[] yValues = {30.0, 10.0, 20.0};
-        TabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
-        Point[] points = TabulatedFunctionOperationService.asPoints(function);
-        assertEquals(3.0, points[0].x, 1e-10);
-        assertEquals(30.0, points[0].y, 1e-10);
-        assertEquals(1.0, points[1].x, 1e-10);
-        assertEquals(10.0, points[1].y, 1e-10);
-        assertEquals(2.0, points[2].x, 1e-10);
-        assertEquals(20.0, points[2].y, 1e-10);
+        assertThrows(ArrayIsNotSortedException.class, () -> {
+            TabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
+        });
+        Exception exception = assertThrows(ArrayIsNotSortedException.class, () -> {
+            new ArrayTabulatedFunction(xValues, yValues);
+        });
+    }
+    @Test
+    void testAddWithSameTypeFunctions() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {2.0, 4.0, 6.0};
+        TabulatedFunction func1 = new ArrayTabulatedFunction(xValues1, yValues1);
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+        TabulatedFunction func2 = new ArrayTabulatedFunction(xValues2, yValues2);
+        TabulatedFunction result = service.add(func1, func2);
+        assertEquals(3, result.getCount());
+        assertEquals(3.0, result.getY(0), 1e-10);
+        assertEquals(6.0, result.getY(1), 1e-10);
+        assertEquals(9.0, result.getY(2), 1e-10);
+    }
+
+    @Test
+    void testAddWithDifferentTypeFunctions() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {2.0, 4.0, 6.0};
+        TabulatedFunction func1 = new ArrayTabulatedFunction(xValues1, yValues1);
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+        TabulatedFunction func2 = new LinkedListTabulatedFunction(xValues2, yValues2);
+        TabulatedFunction result = service.add(func1, func2);
+        assertEquals(3, result.getCount());
+        assertEquals(3.0, result.getY(0), 1e-10);
+        assertEquals(6.0, result.getY(1), 1e-10);
+        assertEquals(9.0, result.getY(2), 1e-10);
+    }
+    @Test
+    void testSubtractWithDifferentTypeFunctions() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 8.0, 11.0};
+        TabulatedFunction func1 = new ArrayTabulatedFunction(xValues1, yValues1);
+
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {2.0, 3.0, 4.0};
+        TabulatedFunction func2 = new LinkedListTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunction result = service.subtract(func1, func2);
+
+        assertEquals(3, result.getCount());
+        assertEquals(3.0, result.getY(0), 1e-10);
+        assertEquals(5.0, result.getY(1), 1e-10);
+        assertEquals(7.0, result.getY(2), 1e-10);
     }
 }
